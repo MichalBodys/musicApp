2af5b7f64fcbc59d4aca53b1a2edc50000e5a988
MichalBodys <bodysmic@gmail.com>
2023-06-06 14:49:11 +0200

    guarding routes

--- musicApp/src/router/index.js
+++ musicApp/src/router/index.js
@@ -2,19 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/Home.vue"
 import About from "@/views/About.vue"
 import Manage from "@/views/Manage.vue"
+import error from "@/views/error.vue"
+
+
 
 const routes = [
   {
+    name: 'home',
     path: "/",
     component: Home,
   },
   {
+    name: 'about',
     path: "/about",
     component: About,
   },
   {
-    path: "/manage",
+    name: 'manage',
+    // alias:'/manage',
+    path: "/manage-music",
     component: Manage,
+    beforeEnter: (to, from,next) => {
+      console.log('manage route guard')
+      next()
+
+    }
+  },
+  {
+    path: '/manage',
+    redirect: {name: 'manage'}
+  },
+  {
+    name: 'error',
+    path: '/:catchAll(.*)*',
+    component: error,
   }
 ];
 
@@ -22,8 +43,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-orange-500'
 
 
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('Global Guard')
+
+next()
 })
 
 export default router
---
+++ musicApp/src/views/error.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="text-white flex justify-center items-center mt-10"><p>Something went Wrong back to  <router-link class="font-bold" :to="{ name: 'home'}">Home page</router-link></p></div>
+</template>
